1a8bcc7f66ed45c1cef06053b6b84750488da509
Nico Mastrandrea <nick88msn@gmail.com>
2020-10-13 15:28:33 +0200

    updated with systemd service

--- dotfiles_backup.sh
+++ dotfiles_backup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 
+HOME=/home/evilsocket
+
 cd $HOME
 cp -avr .vimrc $HOME/repo/dotfiles/
 cp -avr .bashrc $HOME/repo/dotfiles/
@@ -7,6 +9,10 @@ cp -avr .Xresources $HOME/repo/dotfiles/
 cp -avr .i3/ $HOME/repo/dotfiles/
 cp -avr .dmenurc $HOME/repo/dotfiles/
 cp -avr $HOME/scripts/dotfiles_backup.sh  $HOME/repo/dotfiles/
+cp -avr $HOME/scripts/dotfiles_backup.service $HOME/repo/dotfiles/
 
 pacman -Qqen > $HOME/repo/dotfiles/pkglist.txt
 yay -Qqen > $HOME/repo/dotfiles/yaylist.txt
+
+cd $HOME/repo/dotfiles
+git add . && git commit -m 'update pushed by system' && git push
