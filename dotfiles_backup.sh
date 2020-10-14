fa0329bcc02d0edec5984ecc8592f02bbccf50d4
Nico Mastrandrea <nick88msn@gmail.com>
2020-10-14 11:07:32 +0200

    regular update

--- dotfiles_backup.sh
+++ dotfiles_backup.sh
@@ -12,7 +12,3 @@ cp -avr $HOME/scripts/dotfiles_backup.sh  $HOME/repo/dotfiles/
 cp -avr $HOME/scripts/dotfiles_backup.service $HOME/repo/dotfiles/
 
 pacman -Qqen > $HOME/repo/dotfiles/pkglist.txt
-yay -Qqen > $HOME/repo/dotfiles/yaylist.txt
-
-cd $HOME/repo/dotfiles
-git add . && git commit -m 'update pushed by system' && git push
